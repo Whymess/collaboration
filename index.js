5a02947ae138b2c0d7aa474a4603fa9958d13460
Whymess <Jonathan.lev.messing@gmail.com>
2017-12-04 12:14:38 -0500

    Fixed

--- index.js
+++ index.js
@@ -6,18 +6,47 @@ let args = process.argv.slice(2);
 
 let file = args;
 
-const fileList = args.map((item) => {
+const fileList = args.filter((item) => {
   return item != ',' && item != '$' && item != '|'
 })
 
-const delimeter = args.map((item) => {
+const delimeter = args.filter((item) => {
   return item == ',' || item == '$' || item == '|'
 })
 
-if(fileList.length !== 3 || delimeter.length !== 3){
-	console.log("Please pass in correct args")
-	process.exit()
+
+if(fileList.length !== 3){
+	console.log("Please enter 3 source files")
+} 
+
+if(delimeter.length !== 3){
+	console.log("Please enter 3 delimeters")
+}
+
+if(delimeter.length === 3 && fileList.length === 3){
+	console.log(fileList, "fileList")
+    console.log(delimeter, "delimeter")
 }
 
 
-console.log(fileList.length )
+
+
+// console.log(fileList[0], "files")
+
+// if(fileList.length !== 3 || delimeter.length !== 3){
+// 	console.log("Please pass in correct args")
+// 	process.exit()
+// }
+
+
+
+// fileList.forEach((file, index) => {
+//    const fileDelimeter = delimeter[index];
+//    console.log(fileDelimeter)
+//    // fs.createReadStream(file).pipe(parse({
+//    //    delimeter: fileDelimeter
+//    // }, (err, data) => {
+//    //     console.log("error");
+//    // }));
+
+// });
